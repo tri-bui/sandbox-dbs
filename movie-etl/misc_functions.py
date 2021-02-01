514049fd2bf11cd3ef03567059cd10587d549dd6
tri-bui <buitri91@gmail.com>
2021-02-01 12:06:24 -0800

    Add drop_redundant_cols function

--- movie-etl/misc_functions.py
+++ movie-etl/misc_functions.py
@@ -37,5 +37,73 @@ def drop_duplicates(movies_df):
     return movies_df
 
 
+def filla_dropb(cola, colb, movies_df):
+
+    """
+    Fill missing values in column a (keep) using values in column b (drop), 
+    then drop column b from the joined movie data.
+
+    Parameters
+    ----------
+    cola : Pandas series
+        Column to fill and keep
+    colb : Pandas series
+        Colunmn to drop
+    movies_df : Pandas dataframe
+        Joined movie data
+
+    Returns
+    -------
+    Pandas dataframe
+        Movie data with missing values of `cola` filled and `colb` dropped
+    """
+    
+    # Helper function to fill missing `cola` values with `colb` values
+    def filla(row):
+        cond1 = pd.isnull(row[cola]) or row[cola] == 0 # `cola` is 0 or missing
+        cond2 = pd.notnull(row[colb]) and row[colb] != 0 # `colb` has a value
+        return row[colb] if cond1 and cond2 else row[cola]
+            
+    # Fill `cola` missing values and drop `colb`
+    movies_df[cola] = movies_df.apply(filla, axis=1)
+    movies_df.drop(colb, axis=1, inplace=True)
+    return movies_df
 
 
+def drop_redundant_cols(movies_df):
+
+    """
+    Drop the following redundant columns:
+    [1] `release_data_wiki` - after dropping the outlier
+    [2] `revenue` - after using it to fill `box_office` missing values
+    [3] `budget_kaggle` - after using it to fill `budget_wiki` missing values
+    [4] `duration` - after using it to fill `runtime` missing values
+
+    Parameters
+    ----------
+    movies_df : Pandas dataframe
+        Joined movie data
+
+    Returns
+    -------
+    Pandas dataframe
+        Movie data with redundant columns dropped
+    """
+
+    # Drop record with `release_date` outlier and `release_date_wiki` column
+    outlier_index = movies_df.loc[(movies_df['release_date_wiki'] > '2000') & 
+                                  (movies_df['release_date_kaggle'] < '1960')].index
+    movies_df.drop(outlier_index, inplace=True)
+    movies_df.drop('release_date_wiki', axis=1, inplace=True)
+
+    # Pairs of redundant columns
+    redundant_pairs = [
+        ['box_office', 'revenue'],
+        ['budget_wiki', 'budget_kaggle'],
+        ['runtime', 'duration']
+    ]
+
+    # Fill the first column and drop the second column for each pair
+    for pair in redundant_pairs:
+        movies_df = filla_dropb(pair[0], pair[1], movies_df)
+    return movies_df
