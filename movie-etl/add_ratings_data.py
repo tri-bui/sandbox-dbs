3b5d427aa6c7922dc88da9f816aa2aa1ea0395c6
tri-bui <buitri91@gmail.com>
2021-02-04 05:42:01 -0800

    Rename add_rating_data function to join_data

--- movie-etl/add_ratings_data.py
+++ movie-etl/add_ratings_data.py
@@ -2,7 +2,39 @@ import numpy as np
 import pandas as pd
 
 
-def add_rating_count(movies_df, ratings_df):
+def reduce_ratings(ratings_df, movie_id, save_path):
+
+    """
+    Convert `timestamp` to datetime and reduce the rating data to only the 
+    movies in the movie data. Save this reduced rating data to a CSV file.
+
+    Parameters
+    ----------
+    ratings_df : Pandas dataframe
+        Kaggle ratings data
+    movie_id : array-like[int]
+        List of `id`s in the movie data
+    save_path : str
+        Path to save the reduced rating data
+
+    Returns
+    -------
+    Pandas dataframe
+        Reduced rating data
+    """
+
+    # Convert timestamp to datettime
+    ratings_df['timestamp'] = pd.to_datetime(ratings_df['timestamp'], unit='s')
+
+    # Filter the rating data to only the movies in the movie data
+    reduced_df = ratings_df[ratings_df['movieId'].isin(movie_id)]
+
+    # Save reduced rating data
+    reduced_df.to_csv(save_path, index=False)
+    return reduced_df
+
+
+def join_data(movies_df, ratings_df):
 
     """
     Aggregate rating counts for each movie and merge the aggregate rating data 
@@ -36,35 +68,3 @@ def add_rating_count(movies_df, ratings_df):
         df[col].fillna(0, inplace=True)
 
     return df
-
-
-def reduce_ratings(ratings_df, movie_id, save_path):
-
-    """
-    Convert `timestamp` to datetime and reduce the rating data to only the 
-    movies in the movie data. Save this reduced rating data to a CSV file.
-
-    Parameters
-    ----------
-    ratings_df : Pandas dataframe
-        Kaggle ratings data
-    movie_id : array-like[int]
-        List of `id`s in the movie data
-    save_path : str
-        Path to save the reduced rating data
-
-    Returns
-    -------
-    Pandas dataframe
-        Reduced rating data
-    """
-
-    # Convert timestamp to datettime
-    ratings_df['timestamp'] = pd.to_datetime(ratings_df['timestamp'], unit='s')
-
-    # Filter the rating data to only the movies in the movie data
-    reduced_df = ratings_df[ratings_df['movieId'].isin(movie_id)]
-
-    # Save reduced rating data
-    reduced_df.to_csv(save_path, index=False)
-    return reduced_df
