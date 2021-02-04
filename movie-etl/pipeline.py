59c4055713afc99b6917dc2b036ec94cb50514b4
tri-bui <buitri91@gmail.com>
2021-02-04 06:52:59 -0800

    Update transform function in pipeline script

--- movie-etl/pipeline.py
+++ movie-etl/pipeline.py
@@ -54,8 +54,37 @@ def extract(file_path, file_type='csv'):
     return data
 
 
-def transform(wiki_movies, kaggle_movies):
+def transform(wiki_movies, kaggle_movies, 
+              data_path=sys_vars.data_path,
+              ratings_file=sys_vars.ratings_file, 
+              reduced_ratings_file=sys_vars.reduced_ratings_file):
 
+    """
+    Clean the movie and rating data, then join them all. Additionally, reduce 
+    the rating data to only movies in the movie data and save this reduced 
+    rating data to a CSV file.
+
+    Parameters
+    ----------
+    wiki_movies : list[dict]
+        Wikipedia movie data in JSON format
+    kaggle_movies : Pandas dataframe
+        Kaggle movie data
+    data_path : str
+        Path to the data directory, by default `data_path` from the 
+        `config.sys_vars` module
+    ratings_file : str
+        Name of the rating data file, by default `ratings_file` from the 
+        `config.sys_vars` module
+    reduced_ratings_file : str
+        Name of the CSV file to save the reduced rating data to, by default 
+        `reduced_ratings_file` from the `config.sys_vars` module
+
+    Returns
+    -------
+    Pandas dataframe
+        Complete data containing movies and aggregate ratings
+    """
 
     # Clean movie data
     wiki_df = udf_wiki.clean_wiki_movies(wiki_movies) # clean Wikipedia data
@@ -63,9 +92,10 @@ def transform(wiki_movies, kaggle_movies):
     movies_df = udf_movies.join_movie_data(wiki_df, kaggle_df) # join movie data
 
     # Extract and reduce rating data
-    ratings_df = extract(sys_vars.data_path + sys_vars.ratings_file)
-    ratings_df = udf_ratings.reduce_ratings(ratings_df, movies_df['movie_id'].values, 
-                                            sys_vars.data_path + sys_vars.reduced_ratings_file)
+    ratings_df = extract(data_path + ratings_file)
+    ratings_df = udf_ratings.reduce_ratings(ratings_df, 
+                                            movies_df['movie_id'].values, 
+                                            data_path + reduced_ratings_file)
 
     # Add aggregate rating counts to the movie data
     df = udf_ratings.join_data(movies_df, ratings_df)
