4089c277890a582f81373b648d5f0b8a082b3ef8
tri-bui <buitri91@gmail.com>
2021-03-04 16:14:39 -0800

    Refactor temp route in hi-weather

--- hi-weather/app.py
+++ hi-weather/app.py
@@ -92,27 +92,32 @@ def temp_stats(start='start', end='end'):
     """ Minimum, average, and maximum temperature over the date range from the 
     start date to the end date """
 
-    # Date range to calculate statistics for
+    # Date range
     start, end = utils.get_date_range(session=session, table=M, 
                                       start_date=start, end_date=end)
 
     # Query the data to calculate the 3 statistics over the date range
-    SELECT = [F.min(M.tobs), F.avg(M.tobs), F.max(M.tobs)]
-    stats = session.query(*SELECT).filter((M.date >= start) & (M.date <= end))
-    stats = stats.first()
+    SELECT = [F.min(M.tobs), F.avg(M.tobs), F.max(M.tobs)] # min, avg, and max `tobs`
+    stats = session.query(*SELECT).filter((M.date >= start) & (M.date <= end)).first()
 
     # Convert query results to JSON
-    stats_json = jsonify(dict(
-        _1_start_date=start, 
-        _2_end_date=end, 
-        _3_min_temp=stats[0], 
-        _4_avg_temp=stats[1], 
-        _5_max_temp=stats[2]
-    ))
+    stats_json = jsonify(
+        Description='Temperature statistics over the date range from the start date to the end date',
+        Directions="""Enter a date (%Y-%m-%d) between 2010-01-01 and 2017-08-23 for "start" and "end" 
+                      in the URL and press enter to see the updated statistics""",
+        Note="""If the start date is left as "start", 2010-01-01 will be used as the start date. 
+                If the end date is left as "end", 2017-08-23 will be used as the end date.""",
+        Sample='http://127.0.0.1:5000/api/v1.0/temp/2015-01-01/2016-12-31',
+        _Data=dict(
+            _1_start_date=start, 
+            _2_end_date=end, 
+            _3_min_temp=stats[0], 
+            _4_avg_temp=stats[1], 
+            _5_max_temp=stats[2]
+        )
+    )
 
-    # Rollback session transaction
-    session.rollback()
-
+    session.rollback() # rollback session transaction before returning
     return stats_json
 
 
